56e0fed25996e317acc3ae6beb3052845101d5dd
mrunalilipte4 <mrunalilipte4@gmail.com>
2020-12-30 10:51:37 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  WeightForm = new FormGroup({
+    Weight: new FormControl('')
+  });
+  collectData(){
+    console.warn(this.WeightForm.value);
+  }
 }
